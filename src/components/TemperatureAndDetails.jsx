5f63129d152583792fc77c38765b22553e5bf1fc
Asad <99832552+AsadNaveed1@users.noreply.github.com>
2023-07-26 01:44:19 +0800

    Create some new components files

---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function TemperatureAndDetails() {
+  return (
+    <div>
+
+      <div className="flex items-center justify-center text-xl py-4 text-cyan-300">
+         <p >CLoudy or Whatever</p>
+      </div>
+
+      <div className="flex flex-row items-center justify-between text-white py-3">
+
+
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default TemperatureAndDetails
--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -27,7 +27,7 @@ function TopButton() {
 
         return <div className='flex items-center justify-around my-6'>
             {cities.map((city)=>(
-                <button key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
+                <button key={city.id} className='text-white text-lg font-medium transition ease-out hover:scale-105'>{city.title}</button>
 
             ))}
         </div>
